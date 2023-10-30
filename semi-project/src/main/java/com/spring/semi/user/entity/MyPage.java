b0cfc945751998f011a3902c46fb0be604d183ee
hansol na <pinecone171@gmail.com>
2023-10-30 20:22:39 +0900

    user mapper 수정

--- semi-project/src/main/java/com/spring/semi/user/entity/MyPage.java
+++ semi-project/src/main/java/com/spring/semi/user/entity/MyPage.java
@@ -1,5 +1,25 @@
 package com.spring.semi.user.entity;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class MyPage {
 
+	private int bno;
+	private String id;
+	private LocalDateTime regDate;
+	private String jobCategory1;
+	private String jobCategory2;
+	private String jobCategory3;
+	private int budget;
 }
