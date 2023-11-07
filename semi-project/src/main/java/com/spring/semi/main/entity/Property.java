fdd934a4895a494b810a9608ec74d2105316620b
SamK5678 <142370028+samkkkkkkk@users.noreply.github.com>
2023-11-07 19:57:31 +0900

    Revert "Develop"

--- semi-project/src/main/java/com/spring/semi/main/entity/Property.java
+++ semi-project/src/main/java/com/spring/semi/main/entity/Property.java
@@ -1,31 +1,22 @@
 package com.spring.semi.main.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter
-@ToString @EqualsAndHashCode
+@Getter @Setter @ToString
+@EqualsAndHashCode
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 // 부동산 엔티티
 public class Property {
-	
-	private int rownum;
-	private String siDO;
-	private String siGunGu;
+
+	private int realestateNo;
 	private String adstrdNm;
 	private int excheGtn;
-	private int area;
 	private double latitude;
 	private double longitude;
-	
-	
-	
-	
 }
